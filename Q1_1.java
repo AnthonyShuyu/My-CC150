7bf34c07b5a3a79ac8dccacb4fd167f7cfb50f59
AnthonyShuyu <ys2530@nyu.edu>
2016-03-13 13:25:11 -0400

    Q1_1

---
+++ Q1_1.java
@@ -0,0 +1,16 @@
+// Determine if a string has all unique characters
+
+public static boolean isUnique(Stirng s) {
+    if (s == null || s.length() == 0) {
+      return true;
+    }
+    int n = s.length();
+    Set<Character> hashSet = new HashSet<Character>();
+    for (int i = 0; i < n; i++) {
+        if (hashSet.contains(s.charAt(i))) {
+            return false;
+        }
+        hashSet.add(s.charAt(i));
+    }
+    return true;
+}
